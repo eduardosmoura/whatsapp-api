29a958cfe9a27cc520b3a5fbdf2c93c20371ef51
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2024-04-05 17:32:25 -0300

    Add axios

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -1,32 +1,26 @@
-const fs = require("fs");
-const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
-const https = require("https");
-function SendMessageWhatsApp(data, number){
+import axios from 'axios';
 
-    const options = {
-        host: "graph.facebook.com",
-        path: `/v18.0/${process.env.PHONE_NUMBER_ID}/messages`,
-        method: "POST",
-        body: data,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-        }
+function SendMessageWhatsApp(data, number) {
+
+    let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`,
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+    data : data
     };
-    const req = https.request(options, res => {
-        res.on("data", d=> {
-            process.stdout.write(d);
+
+    axios
+        .request(config)
+        .then((response) => {
+            console.log(`Message sent to phone number <${number}>:\n`, JSON.stringify(response))
+        })
+        .catch((error) => {
+        console.log(error);
         });
-    });
-
-    req.on("error", error => {
-        console.error(error);
-    });
-
-    req.write(data);
-    req.end((resp) => {
-        console.log(`Message sent to phone number <${number}>:\n`, JSON.stringify(resp))
-    });
 }
 
 module.exports = {
