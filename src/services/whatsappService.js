2cd3a0c6ec9f0c9758f85cb1bcdd7d83fa7763b3
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2024-04-07 19:29:30 -0300

    Refactor

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -62,7 +62,7 @@ async function DescribeImageWhatsApp(imageUrl, number) {
                 {
                     role: "user",
                     content: [
-                        { type: "text", text: "Describe the product list containing an estimated expiry date for each product, assuming they were bought today. Please provide the expire date in the future and in the MM/DD/YY format." },
+                        { type: "text", text: "Describe the product list containing an estimated expiry date for each product, assuming they were bought today. Please provide the expire date respecting the MM/DD/YY format." },
                         { type: "image_url", image_url: { "url": imageUrl, "detail": "high" } },
                     ],
                 },
@@ -71,17 +71,17 @@ async function DescribeImageWhatsApp(imageUrl, number) {
         });
         const { content } = chatResponse?.choices?.[0]?.message
         console.log(`${imageUrl} described for number <${number}>:\n` + content);
-        let data = content.replaceAll('e.g., ', '');
-        for (let i = 1; i <= 30; i++) {
-            data = data.replaceAll(`${i}. `, `\n\n*${i})* `);
-        }
         const filtered = [];
-        data.split('.').forEach(line => {
+        const data = content.replaceAll('e.g., ', '').split('.');
+        data.forEach(line => {
             if (line.trim().length > 0) {
                 line.split('\n').forEach(message => {
                     if (message.trim().length > 0) {
                         if (!message.toLowerCase().includes(`i'm sorry`) && !message.toLowerCase().includes('however') && !message.toLowerCase().includes(`i can't`) && !message.toLowerCase().includes('i cannot')) {
-                            const resp = message.replaceAll('23', '24')
+                            let resp = message.replaceAll('23', '24');
+                            for (let i = 1; i <= 30; i++) {
+                                resp = resp.replaceAll(`${i}. `, `\n*${i})* `);
+                            }
                             filtered.push(resp);
                         }
                     }
