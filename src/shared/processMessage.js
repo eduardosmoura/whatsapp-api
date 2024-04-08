eba93d77741509181be22d2614bfafbe7dc85dad
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2024-04-07 21:02:38 -0300

    Fix mongo

--- src/shared/processMessage.js
+++ src/shared/processMessage.js
@@ -14,13 +14,14 @@ async function Process(image, number) {
     try {
         console.log(`Processing your receipt for phone number <${number}> - START`)
         await client.connect();
+        const db = await client.db('receipt-ai');
         const model = whatsappModel.MessageText("Processing your receipt. Please wait...", number);
         await whatsappService.SendMessageWhatsApp(model, number);
         const imageUrl = await whatsappService.SaveImageWhatsApp(image, number);
         const [raw, description] = await whatsappService.DescribeImageWhatsApp(imageUrl, number);
         const message = whatsappModel.MessageText(description.trim().length > 0 ? description : 'Could not process your receipt. Please try uploading a better quality image.', number);
         await whatsappService.SendMessageWhatsApp(message, number);
-        await client.db("receipts").insertOne({ number, imageUrl, raw, description });
+        await db.collection('receipts').insertOne({ number, imageUrl, raw, description });
         console.log(`Processing your receipt for phone number <${number}> - END`)
     } catch (err) {
         console.log(err);
